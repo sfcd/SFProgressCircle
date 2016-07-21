0a0921b508fea3c64b1a82ce2d3951b1d803d98f
Sibagatov Ildar <ildar.sibagatov@softfacade.com>
2016-07-21 16:35:19 +0300

    Initial Commit

---
+++ SFProgressCircle/SFProgressCircle.h
@@ -0,0 +1,19 @@
+//
+//  SFProgressCircle.h
+//  SFProgressCircle
+//
+//  Created by Sibagatov Ildar on 21/07/16.
+//  Copyright © 2016 SFÇD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFProgressCircle.
+FOUNDATION_EXPORT double SFProgressCircleVersionNumber;
+
+//! Project version string for SFProgressCircle.
+FOUNDATION_EXPORT const unsigned char SFProgressCircleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFProgressCircle/PublicHeader.h>
+
+
